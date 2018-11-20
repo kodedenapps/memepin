418c7c2100bd17b179cd10fd029fc7ba22c8fe61
Kody Doherty <kodydoherty@gmail.com>
2018-11-19 21:57:34 -0500

    aws sdk issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
